0c1f398f9fcc5216831b51e872fe1d761490a885
Ataul Munim <hello@ataulm.com>
2017-01-07 19:38:06 +0100

    Fix #1 to avoid nuking the enabled a11y services list

--- mobile/src/main/java/com/ataulm/talkbacktile/Toggler.java
+++ mobile/src/main/java/com/ataulm/talkbacktile/Toggler.java
@@ -1,16 +1,16 @@
 package com.ataulm.talkbacktile;
 
 import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.provider.Settings;
-import android.util.Log;
-import android.widget.Toast;
 
 class Toggler {
 
     private static final String TALKBACK_SERVICE_NAME = "com.google.android.marvin.talkback/.TalkBackService";
+    private static final String COLON_TALKBACK_SERVICE_NAME = ":" + TALKBACK_SERVICE_NAME;
+    private static final String TALKBACK_SERVICE_NAME_COLON = TALKBACK_SERVICE_NAME + ":";
     private static final String VALUE_DISABLED = "0";
     private static final String VALUE_ENABLED = "1";
+    private static final String EMPTY_STRING = "";
 
     private final ContentResolver contentResolver;
     private final Callback callback;
@@ -21,9 +21,13 @@ class Toggler {
     }
 
     void enableTalkBack() {
-        // TODO: be cool - don't disable the other a11y services
         try {
-            Settings.Secure.putString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, TALKBACK_SERVICE_NAME);
+            String enabledServices = Settings.Secure.getString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (enabledServices.contains(TALKBACK_SERVICE_NAME)) {
+                return;
+            }
+
+            Settings.Secure.putString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, enabledServices + COLON_TALKBACK_SERVICE_NAME);
             Settings.Secure.putString(contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED, VALUE_ENABLED);
         } catch (SecurityException se) {
             callback.onSecurityExceptionThrown();
@@ -31,9 +35,20 @@ class Toggler {
     }
 
     void disableTalkBack() {
-        // TODO: be cool - don't disable all a11y services
-        Settings.Secure.putString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "");
-        Settings.Secure.putString(contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED, VALUE_DISABLED);
+        String enabledServices = Settings.Secure.getString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+        if (!enabledServices.contains(TALKBACK_SERVICE_NAME)) {
+            return;
+        }
+
+        String servicesWithoutTalkBack = enabledServices
+                .replace(COLON_TALKBACK_SERVICE_NAME, EMPTY_STRING)
+                .replace(TALKBACK_SERVICE_NAME_COLON, EMPTY_STRING)
+                .replace(TALKBACK_SERVICE_NAME, EMPTY_STRING);
+
+        Settings.Secure.putString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, servicesWithoutTalkBack);
+
+        String value = servicesWithoutTalkBack.isEmpty() ? VALUE_DISABLED : VALUE_ENABLED;
+        Settings.Secure.putString(contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED, value);
     }
 
     interface Callback {
